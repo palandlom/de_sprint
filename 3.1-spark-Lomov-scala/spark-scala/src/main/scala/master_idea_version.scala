5c3f84d08c544c6693e08addb6c444be99590bf7
palandlom <palandlom@gmail.com>
2022-12-06 21:30:50 +0300

    Reload add solution of 3.3 spark-command tasks

--- 3.1-spark-Lomov-scala/spark-scala/src/main/scala/master_idea_version.scala
+++ 3.1-spark-Lomov-scala/spark-scala/src/main/scala/master_idea_version.scala
@@ -12,7 +12,7 @@ import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions._
 import scala.math.round
 
-object master_old {
+object master_idea_version {
   def main(args: Array[String]): Unit = {
     println("Hello Scala")
 
